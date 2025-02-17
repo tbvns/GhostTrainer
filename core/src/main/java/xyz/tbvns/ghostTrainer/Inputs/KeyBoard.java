470e3e42338686b4fd2d34adb5620696edc109d0
tbvns <tbvns601@gmail.com>
2025-02-17 20:26:11 +0000

    Revert the F2 thingy

--- core/src/main/java/xyz/tbvns/ghostTrainer/Inputs/KeyBoard.java
+++ core/src/main/java/xyz/tbvns/ghostTrainer/Inputs/KeyBoard.java
@@ -3,7 +3,6 @@ package xyz.tbvns.ghostTrainer.Inputs;
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
-import xyz.tbvns.ghostTrainer.Ai.ScreenGuesser;
 import xyz.tbvns.ghostTrainer.Game.AimeTrainerRenderer;
 import xyz.tbvns.ghostTrainer.Game.BallManager;
 import xyz.tbvns.ghostTrainer.Main;
@@ -18,12 +17,12 @@ public class KeyBoard implements NativeKeyListener {
     @Override
     public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
         if (nativeKeyEvent.getKeyCode() == 60) {
-            if (ScreenGuesser.getScreen().equals("inGame")) {
-                AimeTrainerRenderer.setReset(true);
-                Main.show = !Main.show;
-            } else {
-                Ui.showSettings();
-            }
+            AimeTrainerRenderer.setReset(true);
+            Main.show = !Main.show;
+        }
+
+        if (nativeKeyEvent.getKeyCode() == 62) {
+            Ui.showSettings();
         }
 
         if (nativeKeyEvent.getKeyCode() == 56) {
